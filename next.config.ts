cc87dfef285097354b897b6bca51965b266f4276
annierome1 <112560966+annierome1@users.noreply.github.com>
2025-03-30 21:25:38 -0400

    update

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
-        pathname: "/**", // matches all image paths
+        pathname: "/**", 
       },
     ],
   },
