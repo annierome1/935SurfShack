d62fcd7248e10ec917591820c79b6152383d75fc
annierome1 <112560966+annierome1@users.noreply.github.com>
2025-04-25 19:21:58 -0400

    updates

--- components/Layout.js
+++ components/Layout.js
@@ -1,42 +1,21 @@
 // components/Layout.js
-import Head from 'next/head'
-import Header from './Header'
-import Footer from './Footer'
+import Header from './Header';
+import Footer from './Footer';
 
 export default function Layout({ children }) {
   return (
     <>
-      <Head>
-        {/* Preconnect for performance */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        {/* Load Pacifico + Lobster Two */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Pacifico&family=Lobster+Two&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
       <Header />
-
-      <main style={mainStyle}>
-        {children}
-      </main>
-
+      <main style={mainStyle}>{children}</main>
       <Footer />
     </>
-  )
+  );
 }
-
 const mainStyle = {
-  backgroundColor: '#fff',
-  width: '100%',
-  minHeight: '100vh',
-  margin: '0 auto',
-  fontFamily: "'Pacifico', 'Lobster Two', cursive",  // now both are available
-  overflowX: 'hidden'
-}
+  backgroundColor: '#fff', // set your desired background color
+  width: '100%',             // full width of the viewport
+  minHeight: '100vh',        // ensure it covers the full viewport height
+  margin: '0 auto',  
+  fontFamily: 'Lobster2, cursive', 
+  overflowX: 'hidden'       
+};
