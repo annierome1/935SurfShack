feaa1113d09e3f9a2508fb7a2ede28d9b2a03623
annierome1 <112560966+annierome1@users.noreply.github.com>
2025-04-25 19:18:14 -0400

    updates

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,42 @@
 // components/Layout.js
-import Header from './Header';
-import Footer from './Footer';
+import Head from 'next/head'
+import Header from './Header'
+import Footer from './Footer'
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        {/* Preconnect for performance */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        {/* Load Pacifico + Lobster Two */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Pacifico&family=Lobster+Two&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
       <Header />
-      <main style={mainStyle}>{children}</main>
+
+      <main style={mainStyle}>
+        {children}
+      </main>
+
       <Footer />
     </>
-  );
+  )
 }
 
 const mainStyle = {
-  backgroundColor: '#fff', // set your desired background color
-  width: '100%',             // full width of the viewport
-  minHeight: '100vh',        // ensure it covers the full viewport height
-  margin: '0 auto',  
-  fontFamily: 'Pacifico, cursive', 
-  overflowX: 'hidden'       
-};
+  backgroundColor: '#fff',
+  width: '100%',
+  minHeight: '100vh',
+  margin: '0 auto',
+  fontFamily: "'Pacifico', 'Lobster Two', cursive",  // now both are available
+  overflowX: 'hidden'
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
+// pages/_app.js
+import { Lobster_Two } from '@next/font/google'
+const lobster = Lobster_Two({ subsets: ['latin'], weight: ['400'] })
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={lobster.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
