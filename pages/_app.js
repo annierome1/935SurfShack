0da57d36cc635d47abc24528b1b831433f0b97b5
annierome1 <112560966+annierome1@users.noreply.github.com>
2025-04-25 19:24:31 -0400

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 // pages/_app.js
-import { Lobster_Two } from '@next/font/google'
-const lobster = Lobster_Two({ subsets: ['latin'], weight: ['400'] })
+import '../styles/global.css'
+import { Lobster_Two, Pacifico } from 'next/font/google'
 
-export default function App({ Component, pageProps }) {
+const lobster = Lobster_Two({ subsets: ['latin'], weight: ['400'] })
+const pacifico = Pacifico({ subsets: ['latin'], weight: ['400'] })
+
+export default function MyApp({ Component, pageProps }) {
   return (
-    <main className={lobster.className}>
+    <main className={`${lobster.className} ${pacifico.className}`}>
       <Component {...pageProps} />
     </main>
   )
